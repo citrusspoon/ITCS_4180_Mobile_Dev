0b445490cdbfbcc96f36d4f4a12b928fe8011f54
Unknown <citrusspoon@gmail.com>
2017-09-13 12:52:34 -0400

    Coded save button

--- BACLevelCalculator/app/src/main/java/com/example/scott/baclevelcalculator/MainActivity.java
+++ BACLevelCalculator/app/src/main/java/com/example/scott/baclevelcalculator/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.scott.baclevelcalculator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //---------------Global Variables------------------//
+    double weight;
+    boolean gender; //false = female, true = male
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //------------Components-----------------------------------------//
+        final Switch genderSwitch = (Switch) findViewById(R.id.genderSwitch);
+        final EditText weightInput = (EditText)findViewById(R.id.weightInput);
+        final Button saveButton = (Button) findViewById(R.id.saveButton);
+
+
+
+
+        //---------Save Button-----------------------------------------//
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                if(isDouble(weightInput.getText().toString())) {
+                    weight = Double.parseDouble(weightInput.getText().toString());
+
+                }
+                else {
+
+                    Context context = getApplicationContext();
+                    CharSequence text = "Invalid input";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    //Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+                }
+
+
+                gender = genderSwitch.isChecked();
+            }
+        });
+
+
+
+
+    }
+
+    protected boolean isDouble(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }

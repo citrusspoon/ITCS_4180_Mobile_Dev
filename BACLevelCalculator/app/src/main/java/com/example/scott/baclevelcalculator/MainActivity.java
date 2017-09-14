05a4ed1b1f37c5dd0232528e059e30f289b8dbf4
Unknown <citrusspoon@gmail.com>
2017-09-13 21:58:41 -0400

    Various changes, see desc
    Added arraylist to track drinks. Moved BAC calculations into seperate method and changed implementation to work with arraylist.

--- BACLevelCalculator/app/src/main/java/com/example/scott/baclevelcalculator/MainActivity.java
+++ BACLevelCalculator/app/src/main/java/com/example/scott/baclevelcalculator/MainActivity.java
@@ -16,13 +16,39 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    public class Drink {
+
+        double size;
+        double alcPercent;
+
+        public Drink(double size, double alcPercent) {
+            this.size = size;
+            this.alcPercent = alcPercent;
+        }
+
+        public double getSize() {
+            return size;
+        }
+
+        public double getAlcPercent() {
+            return alcPercent;
+        }
+    }
+
+
 
     //---------------Global Variables------------------//
     double weight;
     boolean gender; //false = female, true = male
     final int stepSize = 5; //step interval for alcSlider
     double currentBACLevel = 0.0;
+    ArrayList<Drink> drinkList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +60,13 @@ public class MainActivity extends AppCompatActivity {
         final Button saveButton = (Button) findViewById(R.id.saveButton);
         final SeekBar alcSlider = (SeekBar)findViewById(R.id.alcSlider);
         final TextView alcPercentTrack = (TextView)findViewById(R.id.alcPercentTrack);
-        final ProgressBar bacProgressBar = (ProgressBar)findViewById(R.id.bacProgressBar);
+
         final Button addDrinkButton = (Button) findViewById(R.id.addDrinkButton);
         final RadioGroup drinkRadioGroup = (RadioGroup)findViewById(R.id.drinkRadioGroup);
-        final TextView bacLevelText = (TextView)findViewById(R.id.bacLevel);
-        final TextView statusText = (TextView)findViewById(R.id.statusView);
 
+        final TextView statusText = (TextView)findViewById(R.id.statusView);
+        final ProgressBar bacProgressBar = (ProgressBar)findViewById(R.id.bacProgressBar);
+        final TextView bacLevelText = (TextView)findViewById(R.id.bacLevel);
 
 
 
@@ -61,6 +88,17 @@ public class MainActivity extends AppCompatActivity {
                     toast.show();
                 }
                 gender = genderSwitch.isChecked();
+
+                calculateBac(bacProgressBar, bacLevelText, statusText);
+
+
+                Context context = getApplicationContext();
+                CharSequence text = "Information updated.";
+                int duration = Toast.LENGTH_SHORT;
+
+                //Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
 
@@ -92,17 +130,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 if(currentBACLevel < 0.25) {
+
+                    //get current drink size
                     int currentDrinkID = drinkRadioGroup.getCheckedRadioButtonId(); //gets the id of the currently selected drink size
                     double drinkSize = 0.0;
-                    double drinkAlcPercent = ((double) alcSlider.getProgress()) / 100.0; //alcohol percentage
-                    double bacLevel = 0.0;
-                    double genderConst;
-                    //sets the gender constant
-                    if (gender) //male
-                        genderConst = 0.68;
-                    else //female
-                        genderConst = 0.55;
-
                     switch (currentDrinkID) { //gets the double value of the drink size in oz
                         case R.id.oneOzRadio:
                             drinkSize = 1.0;
@@ -114,13 +145,12 @@ public class MainActivity extends AppCompatActivity {
                             drinkSize = 12.0;
                             break;
                     }
-
-                    bacLevel = ((drinkSize * drinkAlcPercent) * 6.24) / (weight * genderConst);
-                    currentBACLevel += bacLevel;
-
-
-                    bacProgressBar.setProgress((int) (currentBACLevel * 100));
-                    bacLevelText.setText(Double.toString(currentBACLevel).substring(0, 4));
+                    //get current drink %
+                    double drinkAlcPercent = ((double) alcSlider.getProgress()) / 100.0; //alcohol percentage
+                    //add new drink to list
+                    drinkList.add(new Drink(drinkSize,drinkAlcPercent));
+                    //calculate BAC
+                    calculateBac(bacProgressBar, bacLevelText, statusText);
                 }
                 else{
                     Context context = getApplicationContext();
@@ -132,16 +162,7 @@ public class MainActivity extends AppCompatActivity {
                     toast.show();
                 }
 
-                if(currentBACLevel <= 0.08){
-                    statusText.setText("You're safe");
-                }
-                else if(currentBACLevel < 0.2){
-                    statusText.setText("Be careful...");
-                }
-                else if(currentBACLevel <= 0.25){
-                    statusText.setText("Over the limit!");
 
-                }
 
 
 
@@ -150,6 +171,42 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+    }
+
+    protected void calculateBac(ProgressBar p, TextView t, TextView s){
+
+        if(drinkList.size() > 0) {
+            double bacLevel = 0.0;
+            double genderConst;
+            //sets the gender constant
+            if (gender) //male
+                genderConst = 0.68;
+            else //female
+                genderConst = 0.55;
+
+
+            for (Drink d : drinkList) {
+
+                bacLevel = ((d.getSize() * d.getAlcPercent()) * 6.24) / (weight * genderConst);
+                currentBACLevel += bacLevel;
+
+            }
+
+            p.setProgress((int) (currentBACLevel * 100));
+            t.setText(Double.toString(currentBACLevel).substring(0, 4));
+
+            if(currentBACLevel <= 0.08){
+                s.setText("You're safe");
+            }
+            else if(currentBACLevel < 0.2){
+                s.setText("Be careful...");
+            }
+            else{
+                s.setText("Over the limit!");
+
+            }
+        }
 
     }
 

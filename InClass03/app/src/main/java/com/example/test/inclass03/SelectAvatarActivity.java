0c35c6d61ca389ef41c668e1ee1ccafa7ca40e5a
Scott Schreiber <citrusspoon@gmail.com>
2017-09-18 19:34:09 -0400

    Started In Class 03

---
+++ InClass03/app/src/main/java/com/example/test/inclass03/SelectAvatarActivity.java
@@ -0,0 +1,15 @@
+package com.example.test.inclass03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectAvatarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_avatar);
+
+
+    }
+}
